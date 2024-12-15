ac59e9d0aed2a5abcb54c37e8ddef8a0e41075f6
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-15 14:47:18 +0530

    application sweet alert created

--- src/Pages/ApplyJob/ApplyJob.jsx
+++ src/Pages/ApplyJob/ApplyJob.jsx
@@ -1,36 +1,47 @@
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
+import Swal from "sweetalert2";
 
 const ApplyJob = () => {
-      const {user} = useContext(AuthContext)
+      const { user } = useContext(AuthContext)
       const { id } = useParams()
-      const handleApplyJob = e =>{
+      const handleApplyJob = e => {
             e.preventDefault()
-            const form = e.target 
-            const linkedinURL = form.linkedinurl.value 
-            const resume = form.resume.value 
+            const form = e.target
+            const linkedinURL = form.linkedinurl.value
+            const resume = form.resume.value
             const comment = form.comment.value
 
             const jobApplication = {
-                  job_id : id ,
-                  applicantEmail: user.email ,
-                  linkedinURL ,
-                  resume ,
-                  comment ,
+                  job_id: id,
+                  applicantEmail: user.email,
+                  linkedinURL,
+                  resume,
+                  comment,
             }
-            fetch(`${import.meta.env.VITE_URL}/job-application` , {
-                  method :  'POST' ,
+            fetch(`${import.meta.env.VITE_URL}/job-application`, {
+                  method: 'POST',
                   headers: {
-                        'content-type' : 'application/json'
+                        'content-type': 'application/json'
                   },
                   body: JSON.stringify(jobApplication)
             })
-            .then(res => res.json())
-            .then(data => {
-                  console.log(data);
-                  
-            })
+                  .then(res => res.json())
+                  .then(data => {
+                        if (data.insertedId) {
+                              Swal.fire({
+                                    position: "center",
+                                    icon: "success",
+                                    title: "Job application success",
+                                    showConfirmButton: false,
+                                    timer: 1500
+                              });
+
+                              form.reset()
+                        }
+
+                  })
 
       }
       return (
@@ -40,13 +51,13 @@ const ApplyJob = () => {
                               <h1 className=" text-xl md:text-2xl lg:text-3xl font-bold">Apply for Your Dream Job</h1>
                               <p>We’re looking for passionate individuals to join our growing team. Discover exciting opportunities and make an impact with us!</p>
                         </div>
-                       
+
                         <div className=" flex flex-col items-center md:flex-row  gap-5 p-8  rounded-lg shadow-lg shadow-indigo-300 backdrop-blur-2xl">
                               <div>
                                     <img src="https://cdni.iconscout.com/illustration/premium/thumb/online-job-application-illustration-download-in-svg-png-gif-file-formats--portal-apply-cv-resume-business-people-pack-illustrations-4609375.png?f=webp" alt="" />
                               </div>
                               <form onSubmit={handleApplyJob} action="" className=" w-full   text-black flex flex-col gap-3 ">
-                                    
+
                                     <input name="linkedinurl" required placeholder="Enter linkedin URL" type="url" className="input focus:outline-none border-indigo-500 shadow-inner shadow-indigo-200 border-l-4 focus:border-indigo-400 bg-transparent" />
                                     <input name="resume" required placeholder="Enter resume URL (Google drive)" type="url" className="input focus:outline-none border-indigo-500 shadow-inner shadow-indigo-200 border-l-4 focus:border-indigo-400 bg-transparent" />
 
