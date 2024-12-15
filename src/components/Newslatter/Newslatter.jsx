5071e415e47d3fccf3c182b981312f85d14251f7
Mehefuj Ali <mehefujalim@gmail.com>
2024-12-15 11:47:39 +0530

    newslatter updated

--- src/components/Newslatter/Newslatter.jsx
+++ src/components/Newslatter/Newslatter.jsx
@@ -1,14 +1,26 @@
+
 import './newslater.css'
 
+import toast from 'react-hot-toast';
+
 const Newslatter = () => {
+      
+
+      const handleNewsLatterSignIn = (e) => {
+            e.preventDefault();
+            toast.success("Newsletter subscription successful!")
+            e.target.reset()
+      };
+
+
       return (
             <div className=" my-14">
-                  <div className=" container mx-auto h-96 bg-indigo-100 w-full  flex items-center justify-center text-center rounded-xl" id="newslater-contenar">
+                  <div className=" container mx-auto h-96 bg-indigo-700 w-full  flex items-center justify-center text-center rounded-xl" id="newslater-contenar">
                         <div className=" space-y-3 ">
                               <h1 className=" text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-white">Subscribe our newslatter</h1>
                               <p className=" text-gray-50">New Things Will Always Update Regularl</p>
-                              <form className=" join">
-                                    <input type="email" name="" id="" className="input join-item focus:outline-none focus:border-none" />
+                              <form onSubmit={handleNewsLatterSignIn} className=" join">
+                                    <input required placeholder='Enter your email' type="email" name="email" id="" className="input join-item focus:outline-none focus:border-none" />
                                     <button className="btn join-item ">Subscribe</button>
                               </form>
                         </div>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,44 +2,54 @@ import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from "../Firebase/firebase.config";
+// import { signInWithEmailLink } from "firebase/auth/cordova";
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext()
 
-const AuthProvider = ({children}) => {
-      const [user,setUser] = useState(null)
+const AuthProvider = ({ children }) => {
+      const [user, setUser] = useState(null)
       const googleProvider = new GoogleAuthProvider()
-      const googleSignIn  = () => {
-            return signInWithPopup(auth , googleProvider)
+      const googleSignIn = () => {
+            return signInWithPopup(auth, googleProvider)
       }
-      const signOutUser = () =>{
+      const signOutUser = () => {
             return signOut(auth)
       }
-      useEffect(()=>{
-           const unSubscribe = onAuthStateChanged(auth , (currentUser) =>{
+      useEffect(() => {
+            const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
                   setUser(currentUser)
             })
-            return ()=>{
+            return () => {
                   unSubscribe()
             }
-      },[])
-      const emailRegister = (email,password) => {
-            return createUserWithEmailAndPassword(auth,email,password)
+      }, [])
+      const emailRegister = (email, password) => {
+            return createUserWithEmailAndPassword(auth, email, password)
       }
-      const updateUser = (updateData) =>{
-            return updateProfile(auth.currentUser , updateData)
+      const updateUser = (updateData) => {
+            return updateProfile(auth.currentUser, updateData)
       }
-      const emailLogin = (email,password) => {
-            return signInWithEmailAndPassword(auth ,email ,password)
+      const emailLogin = (email, password) => {
+            return signInWithEmailAndPassword(auth, email, password)
       }
+      // const newsLatterSignIn = (email) => {
+            
+      //       console.log(email);
+      //       return signInWithEmailLink(auth, email, window.location.href("https://mail.google.com"))
+                  
+                  
+      // };
+
 
       const userAuth = {
-            user ,
+            user,
             setUser,
             googleSignIn,
             signOutUser,
             emailRegister,
             updateUser,
             emailLogin,
+            // newsLatterSignIn,
       }
       return (
             <AuthContext.Provider value={userAuth} >
